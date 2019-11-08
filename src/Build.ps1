6f9e22ef06fb3992ad452349e8d287d685fa78fd
mrsalty <matteo.cunietti@gmail.com>
2019-11-08 15:40:20 +0000

    package version via build.ps1

---
+++ src/Build.ps1
@@ -0,0 +1,4 @@
+﻿cd src
+dotnet build
+dotnet pack -p:PackageVersion=0.0.{build}
+dotnet nuget push **/Booster.0.0.{build}.nupkg -k oy2k76abznezhudctcvqmiegfvhfdcau5a5l6l36yflkbq -s "https://nuget.org"
