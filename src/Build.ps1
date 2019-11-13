b469b0962d5f848dd1153cb24337a195157b6ccd
mrsalty <matteo.cunietti@gmail.com>
2019-11-13 07:43:10 +0000

    dotnet test

--- src/Build.ps1
+++ src/Build.ps1
@@ -1,5 +1,6 @@
 ﻿cd src
 $build = $Env:APPVEYOR_BUILD_NUMBER
 dotnet build
-dotnet pack -p:PackageVersion=0.0.${build}
-dotnet nuget push **/Booster.0.0.${build}.nupkg -k oy2k76abznezhudctcvqmiegfvhfdcau5a5l6l36yflkbq -s "https://nuget.org"
+dotnet test
+dotnet pack -p:PackageVersion=0.0.$build
+dotnet nuget push **/Booster.0.0.$build.nupkg -k oy2k76abznezhudctcvqmiegfvhfdcau5a5l6l36yflkbq -s "https://nuget.org"
