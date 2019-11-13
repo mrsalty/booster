81f0ee48613a1f7424f6d768f0752f272bc280cc
mrsalty <matteo.cunietti@gmail.com>
2019-11-13 07:35:18 +0000

    try to read `build` env variable (2)

--- src/Build.ps1
+++ src/Build.ps1
@@ -1,4 +1,5 @@
 ﻿cd src
+$build = Get-Variable build -valueOnly
 dotnet build
 dotnet pack -p:PackageVersion=0.0.${build}
 dotnet nuget push **/Booster.0.0.${build}.nupkg -k oy2k76abznezhudctcvqmiegfvhfdcau5a5l6l36yflkbq -s "https://nuget.org"
