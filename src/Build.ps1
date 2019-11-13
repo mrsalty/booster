bbbd21b600875dccf783c68cd000e15d22ffb0b5
mrsalty <matteo.cunietti@gmail.com>
2019-11-13 06:08:04 +0000

    restore Build.ps1 + test docker on appveyor agent

--- src/Build.ps1
+++ src/Build.ps1
@@ -1,4 +1,5 @@
 ﻿cd src
+docker ps
 dotnet build
-dotnet pack -p:PackageVersion=0.0.{build}
-dotnet nuget push **/Booster.0.0.{build}.nupkg -k oy2k76abznezhudctcvqmiegfvhfdcau5a5l6l36yflkbq -s "https://nuget.org"
+dotnet pack 
+dotnet nuget push **/Booster.0.0.**.nupkg -k oy2k76abznezhudctcvqmiegfvhfdcau5a5l6l36yflkbq -s "https://nuget.org"
