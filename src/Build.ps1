cd7d8763558347b59096c3f35ee3c324c649621a
mrsalty <matteo.cunietti@gmail.com>
2019-11-13 07:32:54 +0000

    try to read `build` env variable

--- src/Build.ps1
+++ src/Build.ps1
@@ -1,5 +1,4 @@
 ﻿cd src
-docker ps
 dotnet build
-dotnet pack -p:PackageVersion=0.0.{build}
-dotnet nuget push **/Booster.0.0.{build}.nupkg -k oy2k76abznezhudctcvqmiegfvhfdcau5a5l6l36yflkbq -s "https://nuget.org"
+dotnet pack -p:PackageVersion=0.0.${build}
+dotnet nuget push **/Booster.0.0.${build}.nupkg -k oy2k76abznezhudctcvqmiegfvhfdcau5a5l6l36yflkbq -s "https://nuget.org"
